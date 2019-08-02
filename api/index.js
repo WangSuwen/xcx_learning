22d27b2a855f6020a75cc9f3c30fccd6473e4bc9
wangxuquan <wangxuquan@meituan.com>
2019-08-02 22:10:00 +0800

    Revert "1"
    This reverts commit 1a082cd4e303b59a9a52c8d7af28a6e2178ef1ac.

--- api/index.js
+++ api/index.js
@@ -3,22 +3,22 @@ const { post, get, upload } = require('./core.js');
 module.exports = {
   // 获取用户openId
   getOpenId: function (data) {
-    return post('/verify', data);
+    return post('/api/c/wx/acc/applet/verify', data);
   },
   // 获取城市列表
   getCities: function (data) {
-    return get('/idcardopencities', data);
+    return get('/api/c/inn/idcardopencities', data);
   },
   // 获取酒店列表
   getHotels: function (data) {
-    return post('/seachidcardopen', data);
+    return post('/api/c/inn/seachidcardopen', data);
   },
   // 获取酒店房间列表
   getRooms: function (data) {
-    return get(`/inn/${data}`);
+    return get(`/api/c/room/inn/${data}`);
   },
   // 上传照片
   uploadImg: function (filePath, formData) {
-    return upload('/saveguestpicinfo', filePath, formData);
+    return upload('/api/c/room/saveguestpicinfo', filePath, formData);
   }
 };
